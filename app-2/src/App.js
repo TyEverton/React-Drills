14b587b2571c77895e89805cff1ad27cdbdc659d
Ty Everton <everton_915@yahoo.com>
2021-07-28 15:13:49 -0600

    completed app2 with notes

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'; //imports functions from React
+import './App.css'; //imports styles from css
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { //creates component
+  constructor() { //constructor is basically a state initiator
+  super() //super is used to access variables of its parents class 
+  
+  this.state = { //sets the initial state of the App constructor
+    foods: ["jerky", "chips & salsa", "sushi", "steak", "hot sauce"] //array to be displayed for users
+    }
+  }
+
+  render() { //renders info above to the webpage
+    let foodsToDisplay = this.state.foods //creates an array that maps through the entire array of foods for it to then display each item individually
+    .map((element, index) => {
+      return <h2 key={index}>{element}</h2> //returns individual items based on their index within the foods element
+    })
+
+    return (
+      <div className="App">
+        {foodsToDisplay} 
+      </div>
+    )
 }
 
 export default App;
+
+//line 21 displays the foodsToDisplay element which maps through the entire foods array for each item to be displayed
