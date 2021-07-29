a173bcb2b087b7ff745ab448d91f2a8ed8cd9093
Ty Everton <everton_915@yahoo.com>
2021-07-29 14:02:07 -0600

    attempt at app-4, incomplete

---
+++ app-4/src/Login.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react" //imports reacts functions
+
+class Login extends Component { //beginning line of a component
+  constructor() { //placeholder for initial state
+    super()
+
+    this.state = { //initial state for component
+      username: "", //empty array for username
+      password: "" //empty array for password
+    }
+  }
+
+ this.handleLogin = this.handleLogin.bind(this) 
+} 
+
+handleUsernameChange(name) {
+  this.setState({username: name})
+}
+
+handlePasswordChange(pass) {
+  this.setState({password: pass})
+}
+
+handleLogin() {
+  alert(`Username: ${this.state.username} Password: ${this.state.password}`)
+}
+
+render() {
+  return(
+    <div>
+      <input 
+      onChange={event => this.handleUsernameChange(event.target.value)}
+      type="text"
+      />
+      <input 
+      onChange={event => this.handlePasswordChange(event.target.value)}
+      type="text"
+      />
+      <button onClick={this.handleLogin}>LOGIN</button>
+    </div>
+  )
+}
+
+export default Login
