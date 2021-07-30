54151a3d1386311d49476e452c11f3079a76d44a
Ty Everton <everton_915@yahoo.com>
2021-07-30 15:13:16 -0600

    completed app-5 with updated notes

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import Image from "./Image"
-import './App.css';
+import React, {Component} from 'react'; //imports react library functionality
+import Image from "./Image" //imports the image.js file and statements / component
+import './App.css'; //imports page css
 
-class App extends Component {
-  render () {
-    return (
+class App extends Component { //creates component
+  render () {  //renders component
+    return ( //returns things below to the screen
       <div className="App">
         <Image url={"https://http.cat/409"}/>
       </div>
@@ -12,4 +12,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App; //exports App file for others to import 
+//line 8 displays the "App" file on the screen
+//displays image under specified url
--- app-5/src/Image.js
+++ app-5/src/Image.js
@@ -1,14 +1,17 @@
-import React, {Component} from "react"
+import React, {Component} from "react" //imports react library functionality
 
 class Image extends Component {
   render() {
     return (
       <div>
-        <img src={this.props.url} /> //displays url from app.js 
-        <div>ERROR </div> //displays word error on screen
+        <img src={this.props.url} /> 
+        <div>ERROR </div>
       </div>
     )
   }
 }
 
 export default Image //exports image file for data to be used in other components
+
+//line 7 displays url from app.js 
+//line 8 displays word error on screen
